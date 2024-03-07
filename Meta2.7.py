c7b1e5680bcdbc95521615b942f1139292006b99
OmarValenzuelalazo <159569972+OmarValenzuelalazo@users.noreply.github.com>
2024-03-06 22:45:06 -0800

    Add files via upload

---
+++ Meta2.7.py
@@ -0,0 +1,10 @@
+a = 1
+suma = 0
+while a <= 100:
+    suma=suma+a
+    a=a+1
+print("La suma de los numeros del 1 al 100 es: ",suma)
+numero=int(input("ingresa numero de multiplicar: "))
+for i in range (1,2,3,4,5,6,7,8,9.10,11,12,13):
+    resultado= i * numero
+    print(numero, "x", i, "=", resultado)
